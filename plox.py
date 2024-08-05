d7cf361525fd8b5902b43db8f52975ec4f2cfa65
Andreas Ratchev <andreasratchev@gmail.com>
2024-08-05 12:33:26 +0200

    Set up REPL

---
+++ plox.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+import readline
+from scanner import Scanner
+
+def run(source: str):
+    scanner = Scanner(source)
+    tokens : list[str] = scanner.scan_tokens()
+
+    for token in tokens:
+        print(token)
+
+def run_file(path: str):
+    with open(path, "r") as f:
+        source = f.read()
+        run(source)
+
+def run_prompt():
+    while True:
+        line : str = input("> ")
+        if line == "": break
+        run(line)
+
+def main(argv: list):
+    if len(argv) > 1:
+        print("Usage: plox [script]")
+        sys.exit(64)
+    elif len(argv) == 1:
+        run_file(argv[0])
+    else:
+        run_prompt()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ scanner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Scanner:
+    """plox Scanner/Lexer"""
+
+    def __init__(self, source: str):
+        self.__source = source
+
+    def scan_tokens(self) -> list[str]:
+        return []
