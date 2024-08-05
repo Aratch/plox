bae921cef664aa98e98c407a029a5a646556f5c0
Andreas Ratchev <andreasratchev@gmail.com>
2024-08-05 12:40:11 +0200

    Add error reporting

--- plox.py
+++ plox.py
@@ -4,6 +4,15 @@ import sys
 import readline
 from scanner import Scanner
 
+had_error : bool = False
+
+def report(line: int, where: str, message: str):
+    print(f"[{line}] Error{where}: {message}")
+    had_error = True
+
+def error(line: int, message: str):
+    report(line, "", message)
+
 def run(source: str):
     scanner = Scanner(source)
     tokens : list[str] = scanner.scan_tokens()
@@ -16,11 +25,14 @@ def run_file(path: str):
         source = f.read()
         run(source)
 
+        if had_error: sys.exit(65)
+
 def run_prompt():
     while True:
         line : str = input("> ")
         if line == "": break
         run(line)
+        had_error = False
 
 def main(argv: list):
     if len(argv) > 1:
