96e973511d5aea8da034f4d302eecab26bb6109d
Andreas Ratchev <andreasratchev@gmail.com>
2024-08-06 11:55:37 +0200

    Complete scanner

--- scanner.py
+++ scanner.py
@@ -35,6 +35,25 @@ class Token:
 class Scanner:
     """plox Scanner/Lexer"""
 
+    keywords = {
+        "and" : AND,
+        "class" : CLASS,
+        "else" : ELSE,
+        "false": FALSE,
+        "for" : FOR,
+        "fun": FUN,
+        "if": IF,
+        "nil": NIL,
+        "or": OR,
+        "print": PRINT,
+        "return": RETURN,
+        "super": SUPER,
+        "this": THIS,
+        "true": TRUE,
+        "var": VAR,
+        "while": WHILE
+    }
+
     def __init__(self, source: str):
         self.__source = source
         self.__tokens : list[Token] = list()
@@ -88,7 +107,67 @@ class Scanner:
             case '\n':
                 self.line += 1
 
-            case _: error(self.line, "Unexpected character.")
+            case '"': self.string()
+
+            case _:
+                if self.is_digit(c):
+                    self.number()
+                elif self.is_alpha(c):
+                    self.identifier()
+                else:
+                    error(self.line, "Unexpected character.")
+
+    def is_alpha(self, c: Char) -> bool:
+        return (c >= 'a' and c <= 'z') or \
+            (c >= 'A' and c <= 'Z') or c == '_'
+
+    def is_alphanumeric(self, c: Char) -> bool:
+        return self.is_alpha(c) or self.is_digit(c)
+
+    def is_digit(self, c: Char) -> bool:
+        return c >= '0' and c <= '9'
+
+    def identifier(self):
+        while self.is_alphanumeric(self.peek()):
+            self.advance()
+
+        text: str = self.__source[self.start:self.current]
+        type = Scanner.keywords.get(text)
+
+        if type == None:
+            type = IDENTIFIER
+
+        self.add_token(type)
+
+    def number(self):
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == '.' and self.is_digit(self.peek_next()):
+            self.advance()
+
+            while self.is_digit(self.peek()):
+                self.advance()
+
+        self.add_token(NUMBER,
+                       float(self.__source[self.start:self.current]))
+
+
+    def string(self):
+        while self.peek() != '"' and not self.is_at_end():
+            if peek() == '\n': self.line += 1
+            self.advance()
+
+        if self.is_at_end():
+            error(self.line, "Unterminated string.")
+            return
+
+        # The closing ".
+        self.advance()
+
+        # Trim the surrounding quotes.
+        value = self.__source[self.start+1:self.current-1]
+        self.add_token(STRING, value)
 
     def advance(self) -> Char:
         c : Char = self.__source[self.current]
@@ -98,6 +177,10 @@ class Scanner:
     def peek(self) -> Char:
         if self.is_at_end(): return '\0'
         return self.__source[self.current]
+
+    def peek_next(self) -> Char:
+        if self.current + 1 >= len(self.__source): return '\0'
+        return self.__source[self.current + 1]
 
     def match(self, expected: Char):
         if self.is_at_end(): return False
