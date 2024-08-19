372199c8df2b0481dfc7eec236fa2e5be19de9db
Andreas Ratchev <andreasratchev@gmail.com>
2024-08-19 10:42:07 +0200

    Interpreter up until 7.4.1

--- plox/interpreter.py
+++ plox/interpreter.py
@@ -13,12 +13,47 @@ def is_truthy(obj):
 def is_equal(a, b):
     # This might be superfluous because we don't run the risk of
     # causing a null pointer dereference like in Java
-    if a is None and b is None: return True
-    if a is None: return False
+    if a == None and b == None: return True
+    if a == None: return False
 
     return a == b
 
+class PloxRuntimeError(RuntimeError):
+    def __init__(self, token: Token, message: str) -> None:
+        super().__init__(message)
+        self.token = token
+
+# Unary checker
+def check_number_operand(operator: Token, operand):
+    if isinstance(operand, float): return
+    raise PloxRuntimeError(operator, "Operand must be a number.")
+
+# Binary checker
+def check_number_operands(operator: Token, left, right):
+    if isinstance(left, float) and isinstance(right, float):
+        return
+    raise PloxRuntimeError(operator, "Operands must be numbers.")
+
+def stringify(obj):
+    if object == None: return "nil"
+
+    if isinstance(obj, float):
+        text = str(obj)
+        if text.endswith(".0"):
+            text = text[:-2]
+        return text
+
+    return str(obj)
+
+
 class Interpreter:
+    def interpret(expression: expr):
+        try:
+            value = self.evaluate(expression)
+            print(stringify(value))
+        except PloxRuntimeError:
+            pass
+
     def evaluate(self, expr: Expr):
         return self.visit(expr)
 
@@ -36,6 +71,7 @@ class Interpreter:
 
         match expr.operator.type:
             case TokenType.MINUS:
+                check_number_operand(expr.operator, right)
                 return -float(right)
             case TokenType.BANG:
                 return not is_truthy(right)
@@ -50,24 +86,33 @@ class Interpreter:
 
         match expr.operator.type:
             case TokenType.GREATER:
+                check_number_operands(expr.operator, left, right)
                 return float(left) > float(right)
             case TokenType.GREATER_EQUAL:
+                check_number_operands(expr.operator, left, right)
                 return float(left) >= float(right)
             case TokenType.LESS:
+                check_number_operands(expr.operator, left, right)
                 return float(left) < float(right)
             case TokenType.LESS_EQUAL:
+                check_number_operands(expr.operator, left, right)
                 return float(left) <= float(right)
 
             case TokenType.MINUS:
+                check_number_operands(expr.operator, left, right)
                 return float(left) - float(right)
             case TokenType.PLUS:
                 if isinstance(left, float) and isinstance(right, float):
                     return float(left) + float(right)
                 if isinstance(left, str) and isinstance(right, str):
                     return str(left) + str(right)
+                raise PloxRuntimeError(expr.operator,
+                                       "Operands must be two numbers or two strings.")
             case TokenType.SLASH:
+                check_number_operands(expr.operator, left, right)
                 return float(left) / float(right)
             case TokenType.STAR:
+                check_number_operands(expr.operator, left, right)
                 return float(left) * float(right)
 
             case TokenType.BANG_EQUAL: return False
